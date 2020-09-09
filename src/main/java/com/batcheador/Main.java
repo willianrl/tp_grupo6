dadd412e644de7322a7f5706a34a98ef3ea2f09f
Willian Eduardo Ramirez Lazo <48813828+willianrl@users.noreply.github.com>
2020-09-08 22:33:20 -0300

    primera entrega

--- src/main/java/com/batcheador/Main.java
+++ src/main/java/com/batcheador/Main.java
@@ -1,7 +1,65 @@
 package com.batcheador;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+
+        //JCOMBOBOX
+        String opciones[] = new String[] {"opcion1", "opcion2", "opcion3"};
+
+        JComboBox comboBox = new JComboBox<String>(opciones);
+        comboBox.setBounds(200, 50 ,90,20);
+
+        //TEXTFIELD
+        JTextField textField = new JTextField("Ingresar algo");
+        textField.setBounds(200, 200 , 150, 20);
+
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
+
+                    String texto = textField.getText();
+                    System.out.println(texto);
+                    textField.setText(null);
+                }
+            }
+
+        });
+
+        textField.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                textField.setText(null);
+            }
+        });
+
+        //JBUTTOM
+        JButton buttom = new JButton("boton 1");
+        buttom.setBounds(200,350,90, 20);
+
+        buttom.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                fileChooser.showOpenDialog(null);
+            }
+        });
+
+        frame.add(comboBox);
+        frame.add(textField);
+        frame.add(buttom);
+
+        frame.setSize(600, 700);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        
     }
 }
+
